74485c0211f2d8c252d6ab291b732417a1b79871
oktawn <belyhmitia@.gmail.com>
2024-12-13 16:15:12 +0500

    get jsonData from bungie api

--- src/bungie-api/bungie-api.service.ts
+++ src/bungie-api/bungie-api.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
 import { WeaponEntity } from 'src/entity/weapons.entity';
 import { Repository } from 'typeorm';
 
@@ -17,8 +18,7 @@ export class BungieApiService {
       if (!hasWeapon) {
         throw new BadRequestException('Weapon not found');
       }
-      const id = hasWeapon.id;      
-      console.log(hasWeapon.icon)
+      const id = hasWeapon.id;
       const res = await this.httpService.axiosRef.get(`/Destiny2/Manifest/DestinyInventoryItemDefinition/${id}`);
       if (res.status !== 200) {
         throw new BadRequestException('Weapon not found');
--- src/seeds/weapons.ts
+++ src/seeds/weapons.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { config } from "dotenv";
 import { Knex } from "knex";
 config();
@@ -9,24 +10,41 @@ class ResDto {
     watermark: string;
 }
 
+export async function handlerData(itemType: number) {
+    try {
+        const res = (await axios.get(process.env.WEAPON_URL))
+            .data.Response.jsonWorldComponentContentPaths.en.DestinyInventoryItemDefinition;
+        const jsonData = (await axios.get(process.env.BNG_URL + res)).data;
+        const filterItem = [];
+        for (const item in jsonData) {
+            if (jsonData[item].itemType === itemType) {
+                filterItem.push({
+                    id: item,
+                    name: jsonData[item].displayProperties.name,
+                    icon: jsonData[item].displayProperties.icon,
+                    watermark: jsonData[item].iconWatermark
+                });
+            }
+        }
+        return filterItem;
+    } catch (error) {
+        console.log(error);
+    }
+
+
+}
+
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("weapons").del();
 
     // Inserts seed entries
-    // console.log(process.env.WEAPON_LITE_URL)
-    const res = await fetch(process.env.WEAPON_LITE_URL);
-    if (!res.ok)
-        throw new Error("Failed to fetch weapons");
-
-    const data: ResDto[] = await res.json();
-
+    const data: ResDto[] = await handlerData(3);
     await knex("weapons").insert(data.map(item => ({
         id: item.id,
         name: item.name,
         icon: item.icon,
         watermark: item.watermark
     })));
-
 
 };
